9b223ddd6459a39152126ba07153e1084deb0609
Jonas Charfreitag <jcharfreitag@cs.uni-bonn.de>
2020-10-19 16:13:27 +0200

    raedwrite - TUDataset: Adjacency matrix and graph indicator.

--- networkx/readwrite/tests/test_tud.py
+++ networkx/readwrite/tests/test_tud.py
@@ -1,3 +1,27 @@
 """
     Unit tests for TUD.
 """
+import tempfile
+import networkx as nx
+
+
+class TestTud:
+    @classmethod
+    def setup_class(cls):
+        cls.graphs = []
+
+        g1 = nx.Graph(name="test")
+        e = [("a", "b"), ("b", "c"), ("c", "d"), ("d", "e"), ("e", "f"), ("a", "f")]
+        g1.add_edges_from(e)
+        cls.graphs.append(g1)
+
+        g2 = nx.Graph(name="test")
+        g2.add_edges_from(e)
+        cls.graphs.append(g2)
+
+    def test_one(self):
+        p = tempfile.mkdtemp()
+        nx.write_tud(self.graphs, path=p)
+
+        adj_matrix_file = p + "/DS_A.txt"
+        assert sum(1 for _ in open(adj_matrix_file)) == 2 * 2 * 6
--- networkx/readwrite/tud.py
+++ networkx/readwrite/tud.py
@@ -10,4 +10,44 @@ import networkx as nx
 from networkx.exception import NetworkXError
 from networkx.utils import open_file, not_implemented_for
 
-__all__ = []
+__all__ = ["write_tud"]
+
+
+def write_tud(graphs, path, name="DS", encoding="utf-8"):
+    """Save a collection of graphs in TUDataset format.
+
+        Parameters
+        ----------
+        graphs : NetworkX graphs
+            Iterator or iterable collection of graphs
+
+        path : string or file
+           Filename of directory to store files in
+
+        name : string
+           Name for the dataset
+
+        encoding : string, optional
+           Text encoding.
+    """
+
+    adjacency_matrix_file = f"{path}/{name}_A.txt"
+    graph_indicator_file = f"{path}/{name}_graph_indicator.txt"
+
+    new_graphs = []
+    num_nodes_seen = 0
+
+    with open(adjacency_matrix_file, 'w', encoding=encoding) as amf, open(graph_indicator_file, 'w',
+                                                                          encoding=encoding) as gif:
+        for i, og in enumerate(graphs):
+            assert not og.is_directed()
+            g = og.copy()
+            g = nx.convert_node_labels_to_integers(g, first_label=num_nodes_seen)
+            new_graphs.append(g)
+            for j in range(num_nodes_seen, num_nodes_seen + g.number_of_nodes()):
+                gif.write(f"{i}\n")
+            num_nodes_seen += g.number_of_nodes()
+
+            for u, v in g.edges():
+                amf.write(f"{u} {v}\n")
+                amf.write(f"{v} {u}\n")
